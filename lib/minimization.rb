2300e2beacf089cfa0a0b906486e8c5723dccfbe
Rajat Kapoor <rajat100493@gmail.com>
2014-06-05 12:40:15 +0200

    remove multimin code from old file

--- lib/minimization.rb
+++ lib/minimization.rb
@@ -726,39 +726,3 @@ module Minimization
   puts @num_iter.to_s + "Final State" + x_l.to_s , x_m.to_s , x_u.to_s;
   end
 end
-
-
-  class Multidimensional
-    # Default value for error on function
-    #EPSILON = 1e-6
-    # Default number of maximum iterations
-    #MAX_ITERATIONS = 100
-    # Minimum value for x
-    attr_reader :x_minimum
-    # Minimum value for f(x)
-    attr_reader :f_minimum
-    # Log of iterations. Should be an array
-    attr_reader :log
-    # Name of fields of log
-    attr_reader :log_header
-    # Absolute error on x
-    attr_accessor :epsilon
-    # Expected value. Fast minimum finding if set
-    attr_reader :expected
-    # Numbers of iterations
-    attr_reader :iterations
-    # Create a new minimizer
-    def initialize(lower, upper, proc)
-      @lower = lower
-      @upper = upper
-      @proc = proc
-      @nvars = @proc.arity
-      @params = @proc.parameters
-    end
-
-    def f(*vars)
-      @proc.call(vars)
-    end
-  end
-
-end
